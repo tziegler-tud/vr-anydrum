9b9bacc7ee3129778eb7a99fc183579e8691ece3
Lukas Peschel <lukas.peschel@gmx.de>
2018-05-18 10:45:24 +0200

    added standard deviation, after 100 entries, last 50 entries are calculated as deviation. following entries are corrected by it.

--- AndroidVersion/app/src/main/java/Sensors/AccelerationSensor.java
+++ AndroidVersion/app/src/main/java/Sensors/AccelerationSensor.java
@@ -9,6 +9,8 @@ import android.widget.TextView;
 import com.example.bluefish.anydrum.MainActivity;
 import com.example.bluefish.anydrum.R;
 
+import java.text.DecimalFormat;
+
 import  org.apache.commons.math3.ml.clustering.DBSCANClusterer;
 import  org.apache.commons.math3.stat.descriptive.moment.StandardDeviation;
 
@@ -21,8 +23,10 @@ import java.util.Vector;
 
 public class AccelerationSensor
 {
+    DecimalFormat formatter;
     private DBSCANClusterer clusterer;
     private StandardDeviation stdDeviation;
+    private double stdDeviationValue=0;
     private Sensor mSensor;
     private MainActivity refMain;
     private int updatePeriod = 2 * 1000 * 1000;
@@ -34,7 +38,8 @@ public class AccelerationSensor
 
     public AccelerationSensor(MainActivity refMain, int period)
     {
-        stdDeviation = new StandardDeviation();
+        formatter  = new DecimalFormat("#,###,###.###");
+        stdDeviation = new StandardDeviation(false);
         //clusterer = new DBSCANClusterer(1, 15)
         this.updatePeriod = period;
         this.refMain = refMain;
@@ -54,15 +59,27 @@ public class AccelerationSensor
             public void onSensorChanged(SensorEvent sensorEvent) {
                 // More code goes here
                 System.out.println("zAcl: "+ sensorEvent.values[2]);
-                TextView tv = (TextView) refMain.findViewById(R.id.sensorValue);
-                tv.setText("Acl_Z: "+sensorEvent.values[2]);
+                TextView viewCurrentValue = (TextView) refMain.findViewById(R.id.sensorValue);
+                TextView viewDeviation = (TextView) refMain.findViewById(R.id.viewStdDeviation);
+                TextView viewCount = (TextView) refMain.findViewById(R.id.viewSensorValuesCount);
+
+                viewCurrentValue.setText("Acl_Z: "+formatter.format(sensorEvent.values[2]));
+                viewCount.setText("SensorList: "+listOfSensorData.size());
                 double x = listOfSensorData.get(listOfSensorData.size()-1).getX()+stepX;
                 /*if(listOfSensorData.size() >= 100)
                 {
                     listOfSensorData.clear();
                     x= 0;
                 }*/
-                listOfSensorData.add(new DataPoint(x, sensorEvent.values[2]));
+                if(listOfSensorData.size()==100)
+                {
+                    double[] skalars = new double[100];
+                    for(int i=49; i<100;++i)
+                        skalars[i]=listOfSensorData.get(i).getY();
+                    stdDeviationValue=stdDeviation.evaluate(skalars);
+                    viewDeviation.setText("deviation: "+formatter.format(stdDeviationValue));
+                }
+                listOfSensorData.add(new DataPoint(x, sensorEvent.values[2]-stdDeviationValue));
             }
 
             @Override
