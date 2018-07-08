6e18cc1aec48e4b96b0bb7835c28a0743cafedc0
tziegler-tud <tom.ziegler1@gmx.de>
2018-07-08 17:30:13 +0200

    removed comparison between last and prev element in knock detection

--- AndroidVersion/app/src/main/java/Logic/SensorDataLogic.java
+++ AndroidVersion/app/src/main/java/Logic/SensorDataLogic.java
@@ -21,7 +21,7 @@ public class SensorDataLogic {
 
     public boolean detectKnocks(int currentVal, int prevVal){
 
-            if(currentVal>prevVal+5*this.stdDev && currentVal>4*prevVal){
+            if(currentVal>prevVal+5*this.stdDev){
                 System.out.println(currentVal+"/"+prevVal+"/"+stdDev);
                 return true;
             }
